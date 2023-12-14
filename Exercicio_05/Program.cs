6254c0bcb73d77d342241443f5af5e4feea9b12b
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-14 08:56:05 -0300

    Exercício 5: Calcular fatorial

---
+++ Exercicio_05/Program.cs
@@ -0,0 +1,17 @@
+﻿//Ler um valor N. Calcular e escrever seu respectivo fatorial. Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+// Lembrando que, por definição, fatorial de 0 é 1
+
+Console.Write("Digite um valor: ");
+var n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Fatorial(n));
+
+static int Fatorial(int numero)
+{
+    if (numero == 0)
+    {
+        return 1;
+    }
+
+    return numero * Fatorial(numero - 1);
+}
