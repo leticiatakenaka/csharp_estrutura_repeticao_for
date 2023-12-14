e82bdbfe36607f9de5bf5624ef8051351d7bd8af
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-14 08:50:53 -0300

    Exercicio 4: Divisão por zero

---
+++ Exercicio_04/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Digite um número: ");
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("Digite um par de números separados por espaço: ");
+    var numeros = Console.ReadLine().Split(' ');
+
+    var n1 = double.Parse(numeros[0]);
+    var n2 = double.Parse(numeros[1]);
+
+    if (n2 == 0)
+    {
+        Console.WriteLine("Divisão impossível");
+        continue;
+    }
+
+    Console.WriteLine(n1/n2);
+}
