b72cda6258f1d5efd3b839662b883f4e8d862517
Leticia Santos Takenaka <leticiatakenaka12@gmail.com>
2023-12-14 07:57:48 -0300

    Exercício 1: Números ímpares de 1 a 1000

---
+++ Exercicio_01/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Digite um número de 1 a 1000: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+if (numero is < 1000 and >= 1)
+{
+    for (int i = 1; i <= numero; i++)
+    {
+        if (i % 2 != 0)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
